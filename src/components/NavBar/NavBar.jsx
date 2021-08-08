1a1200995e83d283f5e2ba101ddc42423435f8aa
jadessu <junioradessu@gmail.com>
2021-08-08 16:47:39 -0500

    create a handle click function

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {useState} from 'react'
+import { NavLink, Link, BrowserRouter as Router } from 'react-router-dom'
 import './NavBar.css'
 
 const NavBar = ({ user, handleLogout }) => {
+	const [click, setClick] = useState(false)
+
+	const handleClick = () => setClick(!click)
 	return (
-		<nav>
-			<div>
-			{user ? (
-				<ul>
-					<li>Welcome, {user.name}</li>
-					<li>
-						<NavLink to='' onClick={handleLogout}>Log out</NavLink>
-					</li>
-					<li>
-						<NavLink to="/users">Users</NavLink>
-					</li>
-				</ul>
-			) : (
-				<ul>
-					<li>
-						<NavLink to="/login">Log In</NavLink>
-					</li>
-					<li>
-						<NavLink to="/users">Users</NavLink>
-					</li>
-					<li>
-						<NavLink to="/signup">Sign Up</NavLink>
-					</li>
-				</ul>
-			)}
-			</div>
-		</nav>
+		// <nav>
+		// 	<div>
+		// 	{user ? (
+		// 		<ul>
+		// 			<li>Welcome, {user.name}</li>
+		// 			<li>
+		// 				<NavLink to='' onClick={handleLogout}>Log out</NavLink>
+		// 			</li>
+		// 			<li>
+		// 				<NavLink to="/users">Users</NavLink>
+		// 			</li>
+		// 		</ul>
+		// 	) : (
+		// 		<ul>
+		// 			<li>
+		// 				<NavLink to="/login">Log In</NavLink>
+		// 			</li>
+		// 			<li>
+		// 				<NavLink to="/users">Users</NavLink>
+		// 			</li>
+		// 			<li>
+		// 				<NavLink to="/signup">Sign Up</NavLink>
+		// 			</li>
+		// 		</ul>
+		// 	)}
+		// 	</div>
+		// </nav>
+		<>
+		
+		</>
 	)
 }
 
