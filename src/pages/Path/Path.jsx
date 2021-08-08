05e6f16020ab5be24544bb0fdc038034b73b68c2
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-08-08 16:35:11 -0400

    Path page add

---
+++ src/pages/Path/Path.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// Services
+
+// Components
+
+
+const Path = (props) => {
+  // useStates
+
+  // methods
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Path
