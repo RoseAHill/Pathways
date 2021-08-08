67f575b3b8c120ff89d8e80323ab8043cc7012f5
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-08-08 16:35:38 -0400

    Adds boilerplate pages

---
+++ src/pages/PathsIndex/PathsIndex.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// Services
+
+// Components
+
+
+const PathsIndex = (props) => {
+  // useStates
+
+  // methods
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default PathsIndex
