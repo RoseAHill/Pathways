16cf5873bf8dfe51ba8eb99648626a098ecf1824
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-09 19:57:47 -0400

    creating uef for pathsIndex

--- src/pages/PathsIndex/PathsIndex.jsx
+++ src/pages/PathsIndex/PathsIndex.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 // Services
+import * as pathService from '../../services/pathService' 
 
 // Components
 
 
+
 const PathsIndex = (props) => {
   // useStates
+  const [paths, setPaths] = useState([])
 
   // methods
+
+  useEffect(() => {
+    const fetchPaths = async () => {
+      try{
+        const pathsData = await pathService.getAllPaths()
+        console.log(pathsData)
+      } catch (error){
+        throw error
+      }
+    } 
+    fetchPaths()
+  }, []);
 
   return (
     <>
