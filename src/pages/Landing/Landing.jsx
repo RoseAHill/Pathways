8efcd0152ddffebd6bd27c74f050b687342dd0b3
jadessu <junioradessu@gmail.com>
2021-08-10 11:31:32 -0500

    start styling on landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,15 @@
 import styles from './Landing.module.css'
 import PathCard from '../../components/PathCard/PathCard'
+import Section1 from "./sections/Section1"
 
 const Landing = ({user}) => {
   return (
     <main className={styles.container}>
-      <h1>
+      {/* <h1>
         hello, {user ? user.name : "friend"}
       </h1>
-      <PathCard/>
+      <PathCard/> */}
+      <Section1/>
     </main>
   )
 }
--- src/pages/Landing/sections/section1.jsx
+++ src/pages/Landing/sections/section1.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import "./Section1.css"
 const Section1 = () => {
 
 
