87d9ac6ea738fd0cc4409c6cca7d430bb5576f1f
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-10 13:29:52 -0400

    Stubbing up CreateMilestone

---
+++ src/components/FormComponents/CreateMilestone/CreateMilestone.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const CreateMilestone = (props) => {
+
+    
+    return(
+        <div className="create-milestone">
+
+            <form className="milestone-form" onSubmit={props.handleSubmit} >
+               <div>
+                   <label htmlFor="milestone-title"> Add Milestone </label>
+                   <input
+                   required
+                   autoComplete="off"
+                   placeholder="Milestone Title"
+                   name="milestoneName"
+                   value={props.milestoneName}
+                   onChange={e => props.setMilestoneName(e.target.value)}
+                   id="milestone-title"
+                   /> 
+
+                <label htmlFor="milestone-description"> Milestone Description </label>
+                   <input
+                   required
+                   autoComplete="off"
+                   placeholder="Milestone Description"
+                   name="description"
+                   value={props.description}
+                   onChange={e => props.setDescription(e.target.value)}
+                   id="milestone-description"
+                   />
+
+                   <button type="submit">
+                        Add Milestone
+                   </button>
+               </div>   
+            </form> 
+        </div>
+
+    )
+}
+
+export default CreateMilestone
