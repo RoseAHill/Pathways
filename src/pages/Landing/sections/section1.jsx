bfecaf0502384041a110d29113099108a8105e0d
jadessu <junioradessu@gmail.com>
2021-08-10 09:54:38 -0500

    html for section1

---
+++ src/pages/Landing/sections/section1.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+const Section1 = () => {
+
+
+    return (
+        <section className="home-section" id="home">
+      <div className="container">
+        <div className="row h-100 align-items-center align-content-center">
+          <div className="home-text">
+            <h1>
+              Fall in love with <span>coding</span>
+            </h1>
+            <h2>Explore the world of Competitive Programming.</h2>
+            {/* <a href="#" className="btn btn-1">
+              get started
+            </a> */}
+          </div>
+          <div className="home-img">
+            <div className="home-img-inner">
+              <div className="home-image">
+                <div className="home-objects">
+                {/* <img src={} alt="idea light bulb" /> */}
+                  </div>
+                <div className="home-objects">
+                {/* <img src={} alt="idea light bulb" /> */}
+                  </div>
+                <div className="home-objects">
+                {/* <img src={} alt="idea light bulb" /> */}
+                </div>
+                <div className="home-objects">
+                {/* <img src={} alt="idea light bulb" /> */}
+                </div>
+              </div>
+              {/* <img src={cliff3} alt="home-img" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
+
+export default Section1
+
