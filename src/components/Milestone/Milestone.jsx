f2306bfb0881142ccb8a788eed784a2a95d28f77
Rose Aranaga-Hill <rosearanagahill@gmail.com>
2021-08-10 12:19:52 -0400

    fixed milestone missing map return

--- src/components/Milestone/Milestone.jsx
+++ src/components/Milestone/Milestone.jsx
@@ -4,12 +4,12 @@ import Comment from '../Comment/Comment'
 
 const Milestone = ({ title, description, comments }) => {
   const commentList = comments.map((comment, index) => {
-    <Comment
+    return (<Comment
       key={index}
       content={comment.content}
       refLink={comment.refLink}
       author="Joe Schmoe" //TODO: find author on backend
-    />
+    />)
   })
   
   return (
