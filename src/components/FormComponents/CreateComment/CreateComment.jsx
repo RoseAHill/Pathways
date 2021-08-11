678204ffe7a5decdaf53829290401f9750badeae
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-11 14:59:53 -0400

    adding state

---
+++ src/components/FormComponents/CreateComment/CreateComment.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+//components
+
+const CreateComment = (props) => {
+    const [text, setText] = useState('')
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = {
+
+    }
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+
+
+
+
+
+
+}
