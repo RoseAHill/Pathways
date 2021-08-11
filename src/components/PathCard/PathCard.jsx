3fb8c98deb09e5ae62c1ca192cadd1b8bfabd4b3
jadessu <junioradessu@gmail.com>
2021-08-11 13:51:48 -0500

    create html for path card

--- src/components/PathCard/PathCard.jsx
+++ src/components/PathCard/PathCard.jsx
@@ -15,7 +15,31 @@ const PathCard = (props) => {
 
   return (
     <>
-    
+     <>
+      <div class="path-container">
+        <div class="path-header">
+          <div class="path-cover">
+            <div class="logo">
+              <h3>OUR LOGO</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="path-body">
+          <div class="path-title">
+            <h1>
+              <a href="#">Some info</a>
+            </h1>
+          </div>
+          <div class="path-description">
+            <p>
+              This is the description of the path
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, aliquam impedit! Fugit perferendis excepturi voluptatibus omnis, quidem reprehenderit, vero totam dicta quasi sit aspernatur, quas reiciendis possimus inventore veritatis et?
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
 
     </>
   )
