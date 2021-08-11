3312550ba610927d7007c56775916976127412b3
Sarah Galed <sarajessicagaled@gmail.com>
2021-08-11 15:21:07 -0400

    fixing the form

--- src/components/FormComponents/CreateComment/CreateComment.jsx
+++ src/components/FormComponents/CreateComment/CreateComment.jsx
@@ -1,27 +1,31 @@
+import { text } from 'express';
 import React, { useState } from 'react';
 
 //components
 
 const CreateComment = (props) => {
     const [text, setText] = useState('')
-}
+
 
 const handleSubmit = (e) => {
     e.preventDefault()
     const formData = {
-
+        content: text,
+        author: props.currentUser._id,
+        refLink: text,
+     }
+     props.handleCreateComment(formData)
     }
 
     return(
         <div>
-            
+
         </div>
     )
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
