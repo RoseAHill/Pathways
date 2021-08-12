65e575f1f9a4ad4cbad9ea6b4e812dd2bd84ad58
jadessu <junioradessu@gmail.com>
2021-08-12 09:39:02 -0500

    final changes

--- src/components/Milestone/Milestone.jsx
+++ src/components/Milestone/Milestone.jsx
@@ -1,77 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // services
-import { createComment, deleteComment} from '../../services/commentService'
+import { createComment, deleteComment } from "../../services/commentService";
 
 // components
-import Comment from '../Comment/Comment'
-import CreateComment from '../FormComponents/CreateComment/CreateComment'
+import Comment from "../Comment/Comment";
+import CreateComment from "../FormComponents/CreateComment/CreateComment";
 
+import "./Milestone.css";
 
-import './Milestone.css'
+const Milestone = ({
+  title,
+  description,
+  comments,
+  currentUser,
+  milestoneId,
+}) => {
+  const [commentsArray, setCommentsArray] = useState([...comments]);
 
-const Milestone = ({ title, description, comments, currentUser, milestoneId }) => {
-  
-  const [commentsArray, setCommentsArray] = useState([...comments])
-  
-  
   const handleDeleteComment = async (commentId) => {
     try {
-      await deleteComment(milestoneId, commentId)
-      setCommentsArray(commentsArray.filter(comment => comment._id !== commentId))
+      await deleteComment(milestoneId, commentId);
+      setCommentsArray(
+        commentsArray.filter((comment) => comment._id !== commentId)
+      );
     } catch (error) {
-      throw error
+      throw error;
     }
-  }
-  
+  };
+
   const commentList = commentsArray.map((comment, index) => {
-    return (<Comment
-      key={index}
-      handleDeleteComment={handleDeleteComment}
-      commentId={comment._id}
-      milestoneId={milestoneId}
-      currentUser={currentUser}
-      content={comment.content}
-      refLink={comment.refLink}
-      author={comment.author}
-      />)
-    })
+    return (
+      <Comment
+        key={index}
+        handleDeleteComment={handleDeleteComment}
+        commentId={comment._id}
+        milestoneId={milestoneId}
+        currentUser={currentUser}
+        content={comment.content}
+        refLink={comment.refLink}
+        author={comment.author}
+      />
+    );
+  });
   const handleCreateComment = async (id, formData) => {
     try {
-      const res = await createComment(id, formData)
-      setCommentsArray([...commentsArray, res])
+      const res = await createComment(id, formData);
+      setCommentsArray([...commentsArray, res]);
     } catch (error) {
-      throw error
+      throw error;
     }
-  }
+  };
   return (
-    <>
-    <div className="tab">
-      <input id="tab" type="checkbox" />
+    <div className="milestone">
+      <div className="milestone-info">
+        <h3 className="milestone-title">{title}</h3>
+        <p className="milestone-description">{description}</p>
+      </div>
 
-      {/* <div className="milestone"> */}
-      <label for="tab">
-        <div className="milestone-info">
-          <h3 className="milestone-title">{title}</h3>
-          <p className="milestone-description">{description}</p>
-        </div>
-      </label>
-      <div class="tab-content">
-        <div className="comment-section">
-          <div className="comments-list">{commentList}</div>
-        </div>
+      <div className="comment-section">
+        <div className="comments-list">{commentList}</div>
+      </div>
+      <div className="add-comment">
+        <CreateComment
+          currentUser={currentUser}
+          handleCreateComment={handleCreateComment}
+          milestoneId={milestoneId}
+        />
       </div>
     </div>
-    <div className="add-comment">
-      <CreateComment
-        currentUser={currentUser}
-        handleCreateComment={handleCreateComment}
-        milestoneId={milestoneId}
-      />
-    </div>
-    </>
-    
-  )
-}
+  );
+};
 
-export default Milestone
+export default Milestone;
