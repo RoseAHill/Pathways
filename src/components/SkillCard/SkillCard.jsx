dae4b96104f7844bd0500d0a1a0d2951a957cb54
jadessu <junioradessu@gmail.com>
2021-08-12 10:46:12 -0500

    add styling to skill section

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -12,7 +12,7 @@ const SkillCard = ({ skillName, skillId, skillLevel }) => {
       <div className="skill-card">
         <a href={`/skill/${skillId}`}>
         <div className="skill-img">
-        <img src="https://ca-times.brightspotcdn.com/dims4/default/0090820/2147483647/strip/true/crop/3000x2000+0+0/resize/840x560!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F2c%2F81%2Ffcf6a0a04032869986b92e136c2c%2Fla-times-recipe-database-cooking-newsletter.jpg" alt="course-img"/>
+        <img src="https://istefan.ro/img/hero/web-developer-newsletter-coder-stefan-iordache.jpg" alt="course-img"/>
         </div>
         <div class="skill-info">
         <h4>Level {skillLevel}</h4>
